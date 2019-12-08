fab823bb3099252e34a4ceef4a20a5eb75b583f5
benmayer <ben.mayer@ymail.com>
2019-12-08 16:37:28 +0100

    Google Maps working, commit without API KEY

--- resources/components/GoogleMaps.vue
+++ resources/components/GoogleMaps.vue
@@ -1,6 +1,6 @@
 <template>
   <GmapMap
-    :center="{lat:10, lng:10}"
+    :center="{lat:10, lng:11}"
     :zoom="7"
     map-type-id="terrain"
     style="width: 100%; height: 100%"
@@ -18,14 +18,34 @@
     <GmapMarker
       v-for="(m, index) in markers"
       :key="index"
-      :position="m.position"
+      :position="google && new google.maps.LatLng(m.position.lat, m.position.lng)"
+      :icon="m.icon"
       :clickable="true"
-      :draggable="true"
       @click="center=m.position"
     />
   </GmapMap>
 </template>
 
 <script>
+import { gmapApi } from 'vue2-google-maps'
 
+export default {
+  data: () => ({
+    markers: [{
+      position: {
+        lat: 10.0,
+        lng: 10.0,
+        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+      }
+    }, {
+      position: {
+        lat: 11.0,
+        lng: 11.0
+      }
+    }]
+  }),
+  computed: {
+    google: gmapApi
+  }
+}
 </script>

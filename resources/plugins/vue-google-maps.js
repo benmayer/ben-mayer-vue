849b4a0d9ea674d2b8c4bc6975e23c1619109c40
benmayer <ben.mayer@ymail.com>
2019-12-24 17:25:55 +0100

    Added custom html marker

--- resources/plugins/vue-google-maps.js
+++ resources/plugins/vue-google-maps.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
-const goolgeApiKey = process.env.VUE_APP_GOOGLE_API_KEY
+const goolgeApiKey = process.env.VUE_APP_GOOGLE_API_KEY || ''
 
 Vue.use(VueGoogleMaps, {
   load: {
